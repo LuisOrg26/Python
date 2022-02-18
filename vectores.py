5277ebfc57479be058f74e07c9c488437de368ea
LuisOrg26 <91204064+LuisOrg26@users.noreply.github.com>
2022-02-18 08:12:33 -0600

    Update vectores.py

--- vectores.py
+++ vectores.py
@@ -3,15 +3,19 @@ import matplotlib.pyplot as plt
 rectas = []
 vector = []
 ori = []
+lista = []
+orig = []
 
 num_vector = int(input("¿Cual es el numero de vectores que deseas agregar?"))
 for i in range(num_vector):
-    ori.append([0,0]) 
+    ori.append(0) 
     print("Vector #{}".format(i+1))
     punto1 = list(map(int,input("Ingrese los dos valores del vector con una coma\n").split(",")))
     vector.append(punto1)
 
-origen = np.array(ori)
+orig.append(ori)
+orig.append(ori)
+origen = np.array(orig)
 vectores = np.array(vector)
 fig,ax = plt.subplots(1)
 
